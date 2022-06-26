d52bc5bd71aae75f8efd5ef675e87516618d288c
Roger Sherman <rsherman@linux.com>
2022-06-26 18:21:10 +1000

    TF - subnets - routing table, association, and security group

--- terraform/network.tf
+++ terraform/network.tf
@@ -26,3 +26,29 @@ resource "aws_route_table" "flask-public-route" {
     Name = "flask-vpc"
   }
 }
+
+# Create count of subnets in each az
+resource "aws_subnet" "flask-public-subnets" {
+  vpc_id = aws_vpc.flask-vpc.id
+  count = 2
+  cidr_block = var.subnet_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+
+  tags = {
+    Name = "flask-public-${count.index}"
+  }
+}
+
+# connect subnets with routing table
+resource "aws_route_table_association" "flask-route-table-assoc" {
+  count = 2
+  route_table_id = aws_route_table.flask-public-route.id
+  subnet_id = aws_subnet.flask-public-subnets.*.id[count.index]
+}
+
+# create security group
+resource "aws_security_group" "flask-sg" {
+  name = "needs-better-name"
+  vpc_id = aws_vpc.flask-vpc.id
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,11 @@ variable "vpc_cidr" {
   description = "VPC CIDR Block"
   default     = "10.0.0.0/16"
 }
+
+variable "subnet_cidrs" {
+  description = "Subnet CIDR Blocks"
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
