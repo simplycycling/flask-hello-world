95fff8fc2a581f8847b34e7e01c2c156a7ac5de8
Roger Sherman <rsherman@linux.com>
2022-06-26 17:54:42 +1000

    TF - added cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   profile = "default"
   region = "ap-southeast-2"
 }
+
+resource "aws_ecs_cluster" "flask-cluster" {
+  name = "hello world"
+
+  tags = {
+    Name = "hw-flask"
+  }
+}
