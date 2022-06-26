23d12b5f0ff5556cc8fee9b2d1d5de5c2bcb08b9
Roger Sherman <rsherman@linux.com>
2022-06-26 17:49:39 +1000

    FEATURE - added unit tests

--- hello.py
+++ hello.py
@@ -14,6 +14,20 @@ def hello_again():
     return 'Hiiiii!!!5!!'
 
 
+def test_hello():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Hello World!'
+
+
+def test_hello2():
+    response = app.test_client().get('/hello2')
+
+    assert response.status_code == 200
+    assert response.data == b'Hiiiii!!!5!!'
+
+
 if __name__ == '__main__':
     app.run()
 
